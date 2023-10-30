85d076f0a6e655b0f9f357fc3c965866e419f809
Ashtaggg <alexis1.raynal@gmail.com>
2023-10-30 17:40:44 +0000

    first commit

---
+++ dbconnect.py
@@ -0,0 +1,38 @@
+import pymysql
+
+
+def mysqlconnect():
+
+    # To connect MySQL database. Change the database name as per requirement
+
+    conn = pymysql.connect(
+        host='localhost',
+        user='root',
+        password="",
+        db='projet_python_ars',
+    )
+
+    #Change the name of the table as per requirement
+
+    cur = conn.cursor()
+
+    cur.execute("select * from customer")
+
+    output = cur.fetchall()
+
+    print(output)
+
+
+
+    # To close the connection
+
+    conn.close()
+
+
+
+
+
+# Driver Code
+
+if __name__ == "__main__":
+    mysqlconnect()
