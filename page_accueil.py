bccd8edb6ceb218755e99cb6ce3b859b8a752a6a
Kant1R12 <96302985+Kant1R12@users.noreply.github.com>
2023-11-03 12:08:01 +0000

    page ac

--- page_accueil.py
+++ page_accueil.py
@@ -2,8 +2,23 @@ import pymysql
 from dbconnect import mysqlconnect  # Importez la fonction depuis dbconnect.py
 from dbconnect import cuicui
 import tkinter as tk
+from PIL import Image, ImageTk
 
-label = tk.Label(cuicui, text="Hello, Accueil!")     # Crée un libellé (label)
+
+# Crée un libellé pour le titre de la page d'accueil
+titre_label = tk.Label(cuicui, text="Welcome on Cuicui Airlines", font=("Helvetica", 24))
+titre_label.pack(pady=20)
+
+
+image = Image.open('./photos/photo_airport_01.jpeg')
+
+photo = ImageTk.PhotoImage(image)
+
+label = tk.Label(cuicui, image=photo)
 label.pack()
 
+
+
+# Lance la boucle principale de l'application
 cuicui.mainloop()
+
